f3404d16bc89ba9b4215d26741f357544d146132
Pradheep Raj <pradheepselva111@gmail.com>
2024-11-28 15:43:58 +0530

    added login bg

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -6,6 +6,7 @@ import { IoPerson } from "react-icons/io5";
 import { FaUnlock } from "react-icons/fa";
 import { IoEye,IoEyeOff  } from "react-icons/io5";
 import Spinner from "../Components/Spinner";
+import { loginBG } from "../assets";
 
 const Login = () => {
     const [error, setError] = useState()
@@ -25,7 +26,7 @@ const Login = () => {
             .catch(err => {setError(err.message + ", Please try again!"); setIsLoading(false)})
     };
     return (
-        <div className="w-screen my-32 flex flex-col items-center justify-center text-xl">
+        <div className="w-screen flex flex-col items-center justify-center text-xl select-none" style={{backgroundImage: `url(${loginBG})`}}>
             <div className="bg-blue-100 rounded-lg flex flex-col gap-y-5 shadow-lg">
                 <div className="flex items-center justify-center bg-blue-500 rounded-t-lg font-bold text-2xl py-4 overflow-hidden text-white text-center"><h1>Login</h1><RiLoginCircleLine size={30} className="ml-3 max-h-fit"/></div>
                 <div className="flex flex-col p-6 gap-y-5">
@@ -36,7 +37,7 @@ const Login = () => {
                     <div className="flex items-center justify-between gap-x-4 py-2 px-4 rounded-full bg-white border-blue-500 focus-within:ring-2">
                         <div className="flex gap-4 items-center">
                             <FaUnlock/>
-                            <input onChange={e => setPassword(e.target.value)} onKeyDown={e => e.key==="Enter" && onLogin()} type={`${isPasswordHidden ? "password" : "text"}`} placeholder="Enter username" className=" ring-0 border-0 outline-0" />
+                            <input onChange={e => setPassword(e.target.value)} onKeyDown={e => e.key==="Enter" && onLogin()} type={`${isPasswordHidden ? "password" : "text"}`} placeholder="Enter password" className=" ring-0 border-0 outline-0" />
                         </div>
                         {isPasswordHidden ? <IoEye className="cursor-pointer" onClick={() => setIsPasswordHidden(false)}/> : <IoEyeOff className="cursor-pointer" onClick={() => setIsPasswordHidden(true)}/> }
                     </div>
